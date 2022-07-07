a907a3c547f5c944f94e1d5c3dc4293dac5ed70a
Theodora <theodoramadalina@yahoo.com>
2022-07-07 20:12:55 +0300

    Erori comentarii

--- src/main/java/com/club/fotbal/controller/FotbalController.java
+++ src/main/java/com/club/fotbal/controller/FotbalController.java
@@ -15,28 +15,28 @@ public class FotbalController {
 
     @GetMapping(value = "/Player")
     public String Player(Model model) {
-    Player myPlayer = Player.builder().build();
+    //Player myPlayer = Player.builder().build();
         List<Player> playerList = List.of(
-                /*new Player("Tatarusanu", 3000, "Steaua Bucuresti", "Portar"),
+                new Player("Tatarusanu", 3000, "Steaua Bucuresti", "Portar"),
                 new Player("Messi", 9000, "Barcelona", "Atacant"),
                 new Player("Ronaldo", 9001, "Realmadrid", "Atacant"),
-                new Player("Mutu", 2000, "Otelu Galati", "Fundas")*/);
+                new Player("Mutu", 2000, "Otelu Galati", "Fundas"));
 
-        model.addAttribute("Player", myPlayer);
+        model.addAttribute("Player", playerList);
 
         return "Player";
     }
 
     @GetMapping(value = "/Club")
     public String Club(Model model) {
-    Club myClub = Club.builder().build();
+    //Club myClub = Club.builder().build();
         List<Club> clubList = List.of(
-                /*new Club("Staua Bucuresti", 30, 1),
+                new Club("Steaua Bucuresti", 30, 1),
                 new Club("Barcelona", 20, 4),
                 new Club("Realmadrid", 40, 1),
-                new Club("Otelu Galati", 25, 2)*/);
+                new Club("Otelu Galati", 25, 2));
 
-        model.addAttribute("Club", myClub);
+        model.addAttribute("Club", clubList);
         return "Club";
     }
 }
--- src/main/java/com/club/fotbal/model/Player.java
+++ src/main/java/com/club/fotbal/model/Player.java
@@ -5,6 +5,8 @@ import lombok.*;
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Player {
 
